6ab896a285e5e987a539dd248b827ef9fb7e2050
Estevam Souza <estevamsouzalaureth@gmail.com>
2024-04-30 11:40:19 -0300

    :boom: novas alterações

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='meu-projeto',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'reportlab',
+        # outras dependências do seu projeto
+    ],
+)
